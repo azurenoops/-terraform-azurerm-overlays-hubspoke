43bc955e4862b139317760dd6fbe67884fba4227
John Spinella <jrspinella@users.noreply.github.com>
2023-02-14 12:01:55 -0500

    Updated outputs

--- output.tf
+++ output.tf
@@ -6,11 +6,27 @@ output "hub_resource_group_name" {
   value       = module.mod_hub_network.hub_resource_group_name
 }
 
+output "hub_virtual_network_id" {
+  description = "The ID of the hub virtual network"
+  value       = module.mod_hub_network.hub_virtual_network_id
+}
+
 output "hub_virtual_network_name" {
   description = "The name of the hub virtual network"
   value       = module.mod_hub_network.hub_virtual_network_name
 }
 
 output "hub_virtual_network" {
+  description = "The hub virtual network address space"
   value = module.mod_hub_network.hub_virtual_network_address_space
+}
+
+output "firewall_private_ip" {
+  description = "The ID of the hub firewall private IP"
+  value       = module.mod_hub_network.firewall_private_ip
+}
+
+output "firewall_public_ip" {
+  description = "The ID of the hub firewall public IP"
+  value       = module.mod_hub_network.firewall_public_ip
 }
