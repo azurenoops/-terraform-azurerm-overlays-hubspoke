22cc92ecaff1871b35e7c716f6eff1a9bdc563ee
John Spinella <jrspinella@users.noreply.github.com>
2023-02-15 10:15:47 -0500

    Updates to Naming

--- modules/operational-logging/naming.tf
+++ modules/operational-logging/naming.tf
@@ -15,7 +15,7 @@ data "azurenoopsutils_resource_name" "laws" {
   name          = var.workload_name
   resource_type = "azurerm_log_analytics_workspace"
   prefixes      = [var.org_name, var.use_location_short_name ?  module.mod_azregions.location_short : module.mod_azregions.location_cli]
-  suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "st"])
+  suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "laws"])
   use_slug      = var.use_naming
   clean_input   = true
   separator     = "-"
--- modules/virtual-network-spoke/naming.tf
+++ modules/virtual-network-spoke/naming.tf
@@ -5,7 +5,7 @@ data "azurenoopsutils_resource_name" "vnet" {
   name          = var.workload_name
   resource_type = "azurerm_virtual_network"
   prefixes      = [var.org_name, var.use_location_short_name ?  module.mod_azregions.location_short : module.mod_azregions.location_cli]
-  suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "pip"])
+  suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "vnet"])
   use_slug      = var.use_naming
   clean_input   = true
   separator     = "-"
@@ -15,7 +15,7 @@ data "azurenoopsutils_resource_name" "snet" {
   name          = var.workload_name
   resource_type = "azurerm_subnet"
   prefixes      = [var.org_name, var.use_location_short_name ?  module.mod_azregions.location_short : module.mod_azregions.location_cli]
-  suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "pip"])
+  suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "snet"])
   use_slug      = var.use_naming
   clean_input   = true
   separator     = "-"
@@ -25,7 +25,7 @@ data "azurenoopsutils_resource_name" "nsg" {
   name          = var.workload_name
   resource_type = "azurerm_network_security_group"
   prefixes      = [var.org_name, var.use_location_short_name ?  module.mod_azregions.location_short : module.mod_azregions.location_cli]
-  suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "pip"])
+  suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "nsg"])
   use_slug      = var.use_naming
   clean_input   = true
   separator     = "-"
@@ -35,7 +35,7 @@ data "azurenoopsutils_resource_name" "rt" {
   name          = var.workload_name
   resource_type = "azurerm_route_table"
   prefixes      = [var.org_name, var.use_location_short_name ?  module.mod_azregions.location_short : module.mod_azregions.location_cli]
-  suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "rt"])
+  suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "route-table"])
   use_slug      = var.use_naming
   clean_input   = true
   separator     = "-"
